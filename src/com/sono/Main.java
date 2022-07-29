24331804db426f090742f3b788fc9a12a0d46d8e
tkysn1028 <sono@oysterworld.jp>
2022-07-29 14:51:00 +0900

    changed main

--- src/com/sono/Main.java
+++ src/com/sono/Main.java
@@ -1,13 +1,7 @@
 package com.sono;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
-		System.out.println(list.isEmpty());
-		
-		list.stream().filter(str -> str.length()>0).forEach(t -> System.out.println(t));
+		System.out.println("Hello World!");
 	}
 }
